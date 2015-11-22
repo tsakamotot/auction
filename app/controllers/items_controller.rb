8d19ef59aa39e9765be35b8036ac76253c1038d3
taro sakamoto <t.sakamoto8778@gmail.com>
2015-11-22 13:01:49 +0900

    createの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,4 +8,15 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
+  def create
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to "/items/#{@item.id}"
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :price, :seller_id, :description, :email, :image_url)
+  end
 end
